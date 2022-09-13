2b867ffd208c9058fb2ef210fed0009cd3f59d16
Thiago Pessoa <thiago.pessoa@liferay.com>
2022-09-13 16:58:08 -0300

    Atualizado

---
+++ Java/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+	public static void main(String[] args) {
+		/*System.out.println("Hello World");
+		System.out.println("Não tem nada aqui!");
+		Metodo de saida de dados*/
+		//System.out.println(3+3); está é uma operação matematica 
+
+		//float A = 10.00f;
+		/* variaveis do tipo float, necessitam de uma letra f ao final
+		do valor para indentificar que aquele valor é float */
+		//int B = 10;
+		// Variaveis:float, int, String, char, boolean.
+		//System.out.println(A+B);
+
+
+		int D = 12;
+		int M = 9;
+		int Y = 2022;
+
+		System.out.println("What's the date?");
+		System.out.print(D, M, Y);
+
+
+		/*int Date = 12;
+		Date = 13;
+		System.out.println(Date);*/
+		/*final: ao colocar a palavra final no inicio da declaração
+		da sua variavel, o valor dela torna-se imutal, como no exemplo
+		abaixo. Pois essa palavra declarava que a variavel é final ou constante,
+		o que siginifica imutal e somente leitura. */
+		/*final int Date = 12;
+		Date = 13;
+		System.out.println(Date);*/
+
+		/* int myNum = 5;
+		float myFloatNum = 5.99f;
+		char myLetter = 'D';
+		boolean myBool = true;
+		String myText = "Hello"; */
+
+		
+
+	}	
+}
